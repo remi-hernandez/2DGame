06832e38c62b77ffab2ca825cb5ca7d1b5dbaa3a
Remi Hernandez <remi.hernandez@epitech.eu>
2014-09-12 22:40:18 -0300

    change movements

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -9,22 +9,37 @@ public class PlayerScript : MonoBehaviour
 	/// <summary>
 	/// 1 - La vitesse de déplacement
 	/// </summary>
-	public Vector2 speed = new Vector2(50, 50);
+
+	//public Vector2 speed = new Vector2(50, 50);
 	
 	// 2 - Stockage du mouvement
 	private Vector2 movement;
-	
+	public 	float speed = 8f;
+
 	void Update()
 	{
 		// 3 - Récupérer les informations du clavier/manette
 		float inputX = Input.GetAxis("Horizontal");
 		float inputY = Input.GetAxis("Vertical");
+
 		
 		// 4 - Calcul du mouvement
+		if (inputX > 0) 
+		{
+			transform.Translate(inputX * speed * Time.deltaTime, 0, 0);
+			transform.eulerAngles = new Vector2(0, 0);
+		}
+
+		if (inputX < 0)
+		{
+			transform.Translate(- inputX * speed * Time.deltaTime, 0, 0);
+			transform.eulerAngles = new Vector2(0, 180);
+		}
+		/*
 		movement = new Vector2(
 			speed.x * inputX,
 			speed.y * inputY);
-
+		*/
 		// 5 - Tir
 		bool shoot = Input.GetButtonDown("Fire1");
 		shoot |= Input.GetButtonDown("Fire2");
