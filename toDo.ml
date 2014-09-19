1beb6245be78e9c60416a03fabaef89178b71a34
Remi Hernandez <remi.hernandez@epitech.eu>
2014-09-18 23:16:14 -0300

    add toDo file

---
+++ toDo.ml
@@ -0,0 +1,13 @@
+#Bug to fix : 
+
+	- Ne peux pas tirer en déplacement (conflits entre les scripts?)
+
+#ToDo :
+
+	- Dessinner deux nouveaux éléments de sols
+	- Faire suivre le personnage a la caméra
+
+#Done :
+
+	- Fix bug pour le saut
+	- Faire apparaitre la balle au bon endroit
