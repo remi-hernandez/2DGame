b0ac78c029c1150a765152f534d8236286fce653
Remi Hernandez <remi.hernandez@epitech.eu>
2014-09-21 20:53:28 -0300

    maj toDo.ml

--- toDo.ml
+++ toDo.ml
@@ -6,9 +6,10 @@
 #ToDo :
 
 	- Dessinner deux nouveaux éléments de sols
-	- Faire suivre le personnage a la caméra
+	- Empecher le joueur de pouvoir aller a gauche et sortir de la map
 
 #Done :
 
 	- Fix bug pour le saut
 	- Faire apparaitre la balle au bon endroit
+	- Faire suivre le personnage a la caméra
