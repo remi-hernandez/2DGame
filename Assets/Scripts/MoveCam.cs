ec1287a911f6c225c4ef142df66fb9204e983780
Remi Hernandez <remi.hernandez@epitech.eu>
2014-09-21 21:04:17 -0300

    correct bug camera when player jumps

--- Assets/Scripts/MoveCam.cs
+++ Assets/Scripts/MoveCam.cs
@@ -6,18 +6,20 @@ public class MoveCam : MonoBehaviour
 
    public GameObject Player;
    private Vector3 Pos;
+   private float posY;
 
 	// Use this for initialization
 	void Start()
    {
-	
+      posY = Player.transform.position.y + 3;
 	}
 	
 	// Update is called once per frame
 	void Update()
    {
       Pos.x = Player.transform.position.x + 10; // Correspond au décalage de la caméra pour cadrer le joueur a gauche et un peu vers le bas.
-      Pos.y = Player.transform.position.y + 3;
+      Pos.y = posY;
+      // Pos.y = Player.transform.position.y + 3;
       Pos.z = -10;
 
       transform.position = Pos;
