01d07006381898c12bc1c088a6aead4f45452476
Remi Hernandez <remi.hernandez@epitech.eu>
2014-09-21 21:30:22 -0300

    maj toDo list

--- toDo.ml
+++ toDo.ml
@@ -6,6 +6,8 @@
 #ToDo :
 
 	- Dessinner deux nouveaux éléments de sols
+	- Dessinner un mur pour empécher le joueur d'aller trop loin vers
+		la gauche ou a l'arrivée.
 	- Empecher le joueur de pouvoir aller a gauche et sortir de la map
 
 #Done :
